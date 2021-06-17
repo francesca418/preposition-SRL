1c5e00d4881cb72de57452d2deeec059c41d5853
francesca418 <48101104+francesca418@users.noreply.github.com>
2021-06-17 10:14:49 -0400

    data

---
+++ set_variables.sh
@@ -0,0 +1,9 @@
+export SRL_TRAIN_DATA_PATH="./data/train.txt"
+export SRL_VALIDATION_DATA_PATH="./data/development.txt"
+export SRL_TEST_DATA_PATH="./data/test.txt"
+export CUDA_VISIBLE_DEVICES=0,1
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export OPENMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
